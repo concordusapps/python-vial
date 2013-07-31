ea62670e9e5065bf31b93827958999883ff79a1d
Ryan Leckey <rleckey@concordusapps.com>
2013-07-31 10:29:48 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# vial
+> A session store backed by redis.
+
+## License
+
+Unless otherwise noted, all files contained within this project are liensed under the MIT opensource license. See the included file LICENSE or visit [opensource.org][] for more information.
+
+[opensource.org]: http://opensource.org/licenses/MIT
---
+++ setup.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from os import path
+from pkgutil import get_importer
+
+# Calculate the base directory of the project to get relatives from.
+BASE_DIR = path.abspath(path.dirname(__file__))
+
+# Navigate, import, and retrieve the metadata of the project.
+_imp = get_importer(path.join(BASE_DIR, 'src', 'alchemist'))
+meta = _imp.find_module('meta').load_module('meta')
+
+setup(
+    name='alchemist',
+    version=meta.version,
+    description=meta.description,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3'
+    ],
+    author='Concordus Applications',
+    author_email='support@concordusapps.com',
+    url='http://github.com/concordusapps/python-vial',
+    package_dir={'vial': 'src/vial'},
+    packages=find_packages(path.join(BASE_DIR, 'src')),
+    install_requires=(
+        # Python client for redis, an in-memory database that persists on disk.
+        # <https://github.com/andymccurdy/redis-py>
+        'redis',
+    ),
+)
